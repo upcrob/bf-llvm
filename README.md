cddc32f55edf3a7a323c52201034c1c69741f53a
Rob Upcraft <robert.upcraft@gmail.com>
2017-08-24 19:01:42 -0500

    IR instructions updated for LLVM 4.0.1.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a simple implementation of a [Bf](https://en.wikipedia.org/wiki/Brainfuck) to [LLVM](http://llvm.org/) IR compiler.  It has no practical purpose beyond providing a sample project for playing with LLVM.
 
+This compiler was built and tested with LLVM 4.0.1.
+
 ## Building
 
 This project can be built with the included Makefile.  The executable is called, `bfc`.
--- src/bfc.c
+++ src/bfc.c
@@ -25,9 +25,9 @@ int main(void)
 	printf("\t%%icond = icmp eq i32 %d, 0\n", TAPE_SIZE);
 	printf("\tbr i1 %%icond, label %%doneinit, label %%initloop\n");
 	printf("\tinitloop:\n");
-	printf("\t\t%%countval = load i32* %%countRef\n");
+	printf("\t\t%%countval = load i32, i32* %%countRef\n");
 	printf("\t\t%%countvalInc = add i32 %%countval, 1\n");
-	printf("\t\t%%byteRef = getelementptr i8* %%mem, i32 %%countval\n");
+	printf("\t\t%%byteRef = getelementptr i8, i8* %%mem, i32 %%countval\n");
 	printf("\t\tstore i8 0, i8* %%byteRef\n");
 	printf("\t\tstore i32 %%countvalInc, i32* %%countRef\n");
 	printf("\t\t%%dcond = icmp eq i32 %d, %%countvalInc\n", TAPE_SIZE);
@@ -44,57 +44,57 @@ int main(void)
 			case '>':
 				// increment data pointer
 				printf("\t; increment data pointer\n");
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
 				printf("\t%%offsetInc%d = add i32 %%offset%d, 1\n", r, r);
 				printf("\tstore i32 %%offsetInc%d, i32* %%offsetPtr\n", r);
 				break;
 			case '<':
 				// decrement data pointer
 				printf("\t; decrement data pointer\n");
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
 				printf("\t%%offsetDec%d = sub i32 %%offset%d, 1\n", r, r);
 				printf("\tstore i32 %%offsetDec%d, i32* %%offsetPtr\n", r);
 				break;
 			case '+':
 				// increment the byte at the data pointer
 				printf("\t; increment byte\n");
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
-				printf("\t%%value%d = load i8* %%valuePtr%d\n", r, r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%value%d = load i8, i8* %%valuePtr%d\n", r, r);
 				printf("\t%%valueInc%d = add i8 1, %%value%d\n", r, r);
 				printf("\tstore i8 %%valueInc%d, i8* %%valuePtr%d\n", r, r);
 				break;
 			case '-':
 				// decrement the byte at the data pointer
 				printf("\t; decrement byte\n");
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
-				printf("\t%%value%d = load i8* %%valuePtr%d\n", r, r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%value%d = load i8, i8* %%valuePtr%d\n", r, r);
 				printf("\t%%valueDec%d = sub i8 %%value%d, 1\n", r, r);
 				printf("\tstore i8 %%valueDec%d, i8* %%valuePtr%d\n", r, r);
 				break;
 			case '.':
 				// output the byte at the data pointer
 				printf("\t; output byte\n");
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
-				printf("\t%%value%d = load i8* %%valuePtr%d\n", r, r);
-				printf("\tcall i8 (i8)* @putchar(i8 %%value%d)\n", r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%value%d = load i8, i8* %%valuePtr%d\n", r, r);
+				printf("\tcall i8 (i8) @putchar(i8 %%value%d)\n", r);
 				break;
 			case ',':
 				// input a byte from standard in to the data pointer
-				printf("\t%%input%d = call i8 ()* @getchar()\n", r);
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%input%d = call i8 () @getchar()\n", r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
 				printf("\tstore i8 %%input%d, i8* %%valuePtr%d\n", r, r);
 				break;
 			case '[':
 				// if byte at data pointer is zero, jump to command after matching ']'
 				label++;
 				printf("\t; begin loop %d\n", label);
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
-				printf("\t%%value%d = load i8* %%valuePtr%d\n", r, r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%value%d = load i8, i8* %%valuePtr%d\n", r, r);
 				printf("\t%%cond%d = icmp eq i8 0, %%value%d\n", r, r);
 				printf("\tbr i1 %%cond%d, label %%iszero%d, label %%isnonzero%d\n", r, label, label);
 				printf("\tisnonzero%d:\n", label);
@@ -102,9 +102,9 @@ int main(void)
 			case ']':
 				// if byte at data pointer is non-zero, jump to command after matching '['
 				printf("\t; end loop %d\n", label);
-				printf("\t%%offset%d = load i32* %%offsetPtr\n", r);
-				printf("\t%%valuePtr%d = getelementptr i8* %%mem, i32 %%offset%d\n", r, r);
-				printf("\t%%value%d = load i8* %%valuePtr%d\n", r, r);
+				printf("\t%%offset%d = load i32, i32* %%offsetPtr\n", r);
+				printf("\t%%valuePtr%d = getelementptr i8, i8* %%mem, i32 %%offset%d\n", r, r);
+				printf("\t%%value%d = load i8, i8* %%valuePtr%d\n", r, r);
 				printf("\t%%cond%d = icmp eq i8 0, %%value%d\n", r, r);
 				printf("\tbr i1 %%cond%d, label %%iszero%d, label %%isnonzero%d\n", r, label, label);
 				printf("\tiszero%d:\n", label);
